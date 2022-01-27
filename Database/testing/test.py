fea9ea21326f05aed4eebd25d61c873352f4fdb2
Denis Lidzhiev <denis_l_04@mail.ru>
2022-01-27 23:13:35 +0300

    Useless template file removed.

--- Database/testing/test.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-import inspect
-db_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-print(db_dir, os.path.dirname(db_dir), os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))))
