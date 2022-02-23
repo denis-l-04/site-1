c9f55ee3890865575c07e2afd87ef536c67e89a0
Denis Lidzhiev <denis_l_04@mail.ru>
2022-02-23 23:58:58 +0300

    New structure: corrected database path

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 SECRET_KEY = 'greedywindow123'
 DEBUG = True
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, '/database.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, '/app/database.db')
 
 print('base directory:', BASE_DIR)
 print('database used:', SQLALCHEMY_DATABASE_URI)
